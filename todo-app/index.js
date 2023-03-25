eb5bae370697892855a90d1924f862d525fdc44b
Santhosh-kumarks <125248814+Santhosh-kumarks@users.noreply.github.com>
2023-03-25 15:10:32 +0530

    Update index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,5 +1,48 @@
-const app = require("./app");
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`server listening at port - ${port}`);
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const Sequ = require("Sequ");
+const process = require("process");
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
+const db = {};
+
+let Sequ;
+if (config.use_env_variable) {
+  Sequ = new Sequ(process.env[config.use_env_variable], config);
+} else {
+  Sequ = new Sequ(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      Sequ,
+      Sequ.DataTypes
+    );
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
+
+db.Sequ = Sequ;
+db.Sequ = Sequ;
+
+module.exports = db;
+
